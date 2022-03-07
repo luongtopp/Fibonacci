694d91b56237d13296fa1d6532831e4700c02b13
luongtopp <tranducluong1642002@gmail.com>
2022-03-08 03:26:27 +0700

    Fibonacci 01

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Fibonacci {
+    class Program
+    {
+        public static int RabbitPair(int n)
+        {
+            if(n == 1 || n == 2) {
+                
+                return 1;
+            }
+           return RabbitPair(n-1) + RabbitPair(n-2);
+
+
+        }
+
+        
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine($"Rabbit {RabbitPair(10)} cap");
+
+
+
+        }
+    }
+}
